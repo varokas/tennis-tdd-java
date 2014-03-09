1804014011e2750bc4a66da739f7627d3773d10d
josephyao <dermott@126.com>
2014-03-09 22:59:31 +0800

    Gun can't fire again right after fire

--- src/main/java/tennistdd/reloadcountdown/Gun.java
+++ src/main/java/tennistdd/reloadcountdown/Gun.java
@@ -2,12 +2,16 @@ package tennistdd.reloadcountdown;
 
 public class Gun {
     private final Target target;
+    private boolean alreadyFired;
 
     public Gun(Target target) {
         this.target = target;
     }
 
     public void fire() {
+        if (alreadyFired) return;
+
         this.target.isHit();
+        alreadyFired = true;
     }
 }
--- src/test/java/tennistdd/reloadcountdown/GunTest.java
+++ src/test/java/tennistdd/reloadcountdown/GunTest.java
@@ -3,18 +3,27 @@ package tennistdd.reloadcountdown;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class GunTest {
 
+    Target target = mock(Target.class);
+    Gun gun = new Gun(target);
+
     @Test
     public void gun_can_fire_after_init() {
-        Target target = mock(Target.class);
-        Gun gun = new Gun(target);
-
         gun.fire();
 
         verify(target).isHit();
+    }
+
+    @Test
+    public void gun_cannot_fire_again_right_after_fire() {
+        gun.fire();
+        gun.fire();
+
+        verify(target, times(1)).isHit();
     }
 
 }
